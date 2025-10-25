d6488436c703aa099408f8671a5fd1946be9df58
Katia <katia@Katias-MacBook-Air.local>
2025-10-25 16:46:00 +0200

    cmd not found

--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@ import sys
 
 def main():
     sys.stdout.write("$ ")
+    # Captures the user's command in the "command" variable
+    command = input()
+    print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
