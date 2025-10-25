04ad6afabd182c36ecf59450899c2589ed6f8c54
Katia <katia@Katias-MacBook-Air.local>
2025-10-25 16:48:20 +0200

    input loop

--- app/main.py
+++ app/main.py
@@ -2,9 +2,14 @@ import sys
 
 def main():
     sys.stdout.write("$ ")
+    parse_input()
+    main()
+
+def parse_input():
     # Captures the user's command in the "command" variable
     command = input()
     print(f"{command}: command not found")
+
 
 if __name__ == "__main__":
     main()
