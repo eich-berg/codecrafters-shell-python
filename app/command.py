a0ef47d12f7bf88093f753d6c15fd74a3a4311c8
Katia <katia@Katias-MacBook-Air.local>
2025-10-25 17:27:14 +0200

    add type

--- app/command.py
+++ app/command.py
@@ -11,5 +11,10 @@ class Command:
             sys.exit(0)
         if self.args[0] == "echo":
             print(" ".join(arg for arg in self.args[1:]))
+        if self.args[0] == "type":
+            if self.args[1] in ["exit", "echo", "type"]:
+                print(f"{self.args[1]} is a shell builtin")
+            else:
+                print(f"{self.args[1]}: not found")
         else:
             print(f"{self.command}: command not found")
