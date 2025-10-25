bfd595691c413c264cbb37c32b1063706f3f8423
Katia <katia@Katias-MacBook-Air.local>
2025-10-25 17:18:14 +0200

    add echo

--- app/command.py
+++ app/command.py
@@ -2,10 +2,13 @@ import sys
 
 class Command:
     
-    def __init__(self, arg):
-        self.command = arg
+    def __init__(self, args):
+        self.command = args
+        self.args = args.split()
     
     def cmd_parser(self):
-        if self.command == "exit 0":
+        if self.args[0] == "exit":
             sys.exit(0)
+        if self.args[0] == "echo":
+            print(self.args[1:])
         print(f"{self.command}: command not found")
