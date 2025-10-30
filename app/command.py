bd3bf4216ff3843f7b44b5e564bb689e3b062227
Katia <katia@Katias-MacBook-Air.local>
2025-10-30 11:30:19 +0100

    edit history

--- app/command.py
+++ app/command.py
@@ -27,7 +27,8 @@ class Command:
                 commands.append(current)
 
             # Pass the list of commands to handler
-            handler = Handler(self.args, history=self.history)
+            # handler = Handler(self.args, history=self.history)
+            handler = Handler(self.args)
             handler.handle_pipeline(commands)
             return
         
@@ -45,7 +46,8 @@ class Command:
                 redirect_type = op
                 break
 
-        handler = Handler(command_args, redirect_type, filename, self.history)
+        # handler = Handler(command_args, redirect_type, filename, self.history)
+        handler = Handler(command_args, redirect_type, filename)
 
         
         if command_args[0] in cmd_map:
